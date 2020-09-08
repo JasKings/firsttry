7ff071e878af741ffb10df9b93391dc39915b654
JasKings <70942643+JasKings@users.noreply.github.com>
2020-09-08 15:13:40 +0530

    s

---
+++ Area.py
@@ -0,0 +1,9 @@
+
+x=float(input("Input the radius of the circle"))
+Area=float(3.14*x*x)
+print("Area of the circle")
+print(Area)
+ 
+ 
+
+
---
+++ files.py
@@ -0,0 +1,3 @@
+filename=input("Input the filename:")
+fextens=filename.split(".")
+print("The Extension of the file is:"+(fextens[-1]))
